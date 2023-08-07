c17427193c824cc43bda4eab05313cf27c1d6f60
Michael Stegherr <stegherr@agitos.de>
2023-08-07 19:01:32 +0200

    Completed portfolio.

---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    title: "AuScope Portal",
+    subtitle: "Java, ExtJS",
+    description:
+      "Visualisation of mineral resource data in Australia",
+    image: "https://www.cmca.uwa.edu.au/__data/assets/image/0018/1214226/Auscope_logo_b.jpg",
+    link: "http://portal.auscope.org",
+  },
+  {
+    title: "BrainSuite NET",
+    subtitle: "C++",
+    description:
+      "Video distribution software for operating rooms",
+    image: "https://www.brainlab.com/wp-content/uploads/2014/03/Brainlab_Gallery_BrainlabLogo.jpg",
+    link: "https://www.brainlab.com/",
+  },
+  {
+    title: "BioHotspot",
+    subtitle: "React Native",
+    description:
+      "Ecosystem restoration and conservation marketplace",
+    image: "https://images.squarespace-cdn.com/content/v1/5ca2d3afaadd343de55a3313/1554860587050-G81UBC7YW577ZRDK0P8D/biohotspot%2Blogo%2B1.jpg?format=2500w",
+    link: "https://www.oceanwise.com.au",
+  },
+];
+
+export const skills = ["Java", "Java Script", "React", "React Native", "ExtJS", "C++"];
+
+export const testimonials = [
+  {
+    name: "Mathew Wyatt",
+    company: "AIMS",
+    quote: `
+      I had the pleasure of working alongside Michael at the CSIRO as a software engineer
+      developing spatial data integration infrastructure. During that time, Michael displayed
+      strong work ethic, had great attention to quality and detail (given his background in
+      medical software) and was a reliable team member. I learned a lot from Michael during
+      this time, anttd it was a pleasure working with him.`,
+    image: "./1671672418098.jpg",
+  },
+  {
+    name: "Andrew Rodger",
+    company: "BHP",
+    quote: `
+      I had the pleasure of working alongside Michael at the CSIRO as a software engineer
+      developing spatial data integration infrastructure. During that time, Michael displayed
+      strong work ethic, had great attention to quality and detail (given his background in
+      medical software) and was a reliable team member. I learned a lot from Michael during
+      this time, anttd it was a pleasure working with him.`,
+    image: "./1623904111965.jpg",
+  },
+];
